2ab00dbdc2387df1a4f9793cadbe9059556af407
Tailung1 <ramazsamitashvili11@gmail.com>
2025-03-30 21:49:30 +0400

    routing done

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+
+
+import Invoice from "./pages/Invoice";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Invoice />,
+  },
+]);
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    
+    <RouterProvider router={router}>
+    </RouterProvider>
   </StrictMode>
 );
