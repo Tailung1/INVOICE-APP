c651c008fb77f94c66b7de7b620b36ba61df67b7
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-10 20:33:47 +0400

    make code cleaner in InvoiceCont tsx

--- src/customHooks/useGetInvoice.ts
+++ src/customHooks/useGetInvoice.ts
@@ -1,13 +1,13 @@
-import { useParams } from "react-router-dom";
-import { useInvoice } from "../context/invocieContext";
+import { useParams } from "react-router-dom"
+import { useInvoice } from "../context/invocieContext"
 
 export default function useGetInvoice() {
-  const { invoices } = useInvoice();
-  const { id } = useParams();
+    const {invoices}=useInvoice()
+    const {id}=useParams()
 
-  const invoice = invoices.find(
-    (invoice) => invoice.id === Number(id)
-  );
+    const invoice = invoices.find(
+      (invoice) => invoice.id === Number(id)
+    );
 
-  return invoice;
+    return invoice
 }
